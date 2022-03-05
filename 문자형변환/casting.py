8cb35c825d2db71d62a1318757bdff65b5a19c83
greatgiho <giho.lee.1986@gmail.com>
2022-03-05 21:22:43 +0900

    first

---
+++ 문자형변환/casting.py
@@ -0,0 +1,33 @@
+iNum = 10
+fNum = 3.14
+
+print(iNum)
+print(type(iNum))
+
+print(fNum)
+print(type(fNum))
+
+
+iNum = str(iNum)
+fNum = str(fNum)
+
+print(iNum)
+print(type(iNum))
+
+print(fNum)
+print(type(fNum))
+
+flag = str(True)
+print(flag)
+print(type(flag))
+
+fNum1 = 3.14
+fNum2 = 0.123
+
+print(fNum1+fNum2)
+
+fNum1 = str(3.14)
+fNum2 = str(0.123)
+
+print(fNum1+fNum2)
+
---
+++ 문자형변환/casting2.py
@@ -0,0 +1,15 @@
+var = '100'
+print(var)
+print(type(var))
+
+var = int(var)
+print(var)
+print(type(var))
+
+var2 = float(True)
+print(var2)
+print(type(var2))
+
+var3 = "5"
+print(bool(var3))
+print(type(var3))
